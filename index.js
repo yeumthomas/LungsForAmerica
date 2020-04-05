ad415044d8727e4c94796f68549680ad6b36c4b9
Thomas Yeum <yeum.thomas@gmail.com>
2020-04-04 19:57:58 -0500

    Updated navbar

---
+++ index.js
@@ -0,0 +1,20 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+    //toggling the nav
+    burger.addEventListener('click',()=>{
+        nav.classList.toggle('nav-active');
+
+        //animate navlinks
+        navLinks.forEach((link, index) => {
+            link.style.animation = 'navLinkFade 0.5s ease forwards ${index /7 + 1.5}s';
+
+        });
+
+        burger.classList.toggle('toggle');
+    });
+    
+}
+
+navSlide();
