2be37628e129a698f91632e82cbaaf6878117b68
Thomas Yeum <yeum.thomas@gmail.com>
2020-04-04 23:52:23 -0500

    Added Footer

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const navSlide = () => {
 
         //animate navlinks
         navLinks.forEach((link, index) => {
-            link.style.animation = 'navLinkFade 0.5s ease forwards ${index /7 + 1.5}s';
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index /7 + 0.8}s`;
 
         });
 
